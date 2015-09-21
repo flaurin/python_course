c9280f79d20119be8dba81b189f8cdf69a823810
flaurin <fl@trojkan.se>
2015-09-21 14:08:53 +0200

    Har försökt göra läxan
    Men det är ju så att om inte magisterna hade varit i rummet hade det nog
    inte gått...
    Dessutom är det ju så att det man VERKLIGEN skulle vilja är att få
    programmet att räkna ut och formulera sig efter resultatet.

--- day1/homework/robot_writer.py
+++ day1/homework/robot_writer.py
@@ -2,19 +2,21 @@
 
 """ Se robot_writer.md för instruktioner
 """
-
+kommun = "Säffle"
 total_unemployment_2009 = 5.9
 total_unemployment_2014 = 8.0
 
 def write_story(municipality, unemployment_2009, unemployment_2014):
-    text = "Gör mig till en notis"
-    print(text)
-    # Skriv kod här!
+    change = unemployment_2014-unemployment_2009
+    print("Dateline: %s/Ekot" % (municipality))
+    
+    print("Arbetslösheten i landet utvecklas helt enligt de mest begåvade pognoserna. I till exempel %s har arbetslösheten ändrats med %s procentenheter de senaste åren."% (municipality, change))
+write_story(kommun, total_unemployment_2014, total_unemployment_2009)
 
-
-
-""" Testa roboten!
 """
+    # Skriv kod här!
+    *********************
+
 
 write_story("Stockholm", 7.1, 6.6) 
 print("**************")
@@ -25,6 +27,6 @@ print("**************")
 write_story("Lessebo", 9.5, 13.2) 
 print("**************")
 
-"""
+
 Källa: http://www.ekonomifakta.se/sv/Fakta/Regional-statistik/Din-kommun-i-siffror/
 """
